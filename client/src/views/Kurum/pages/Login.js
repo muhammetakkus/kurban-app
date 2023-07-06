4204d5be7754b471f8a6572a6cde3e09340b7143
Mehmet Akkuş <muhammetakkuss34@gmail.com>
2023-07-06 16:46:02 +0300

    login and register cover images are updated for responsive

--- client/src/views/Kurum/pages/Login.js
+++ client/src/views/Kurum/pages/Login.js
@@ -52,7 +52,7 @@ function Login() {
       <div className="flex items-center p-6 bg-gray-50 dark:bg-gray-900">
           <div className="flex-1 h-full max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-800">
             <div className="flex flex-col overflow-y-auto md:flex-row">
-              <div className="h-32 md:h-auto md:w-1/2">
+              <div className="h-32 md:h-auto md:w-1/2 login-cover-img">
                 <img
                   aria-hidden="true"
                   className="object-cover w-full h-full dark:hidden"
--- client/src/views/Kurum/pages/Register.js
+++ client/src/views/Kurum/pages/Register.js
@@ -68,7 +68,7 @@ function Register() {
         className="flex-1 h-full max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-800"
       >
         <div className="flex flex-col overflow-y-auto md:flex-row">
-          <div className="h-32 md:h-auto md:w-1/2">
+          <div className="h-32 md:h-auto md:w-1/2 register-cover-img">
             <img
               aria-hidden="true"
               className="object-cover w-full h-full dark:hidden"
